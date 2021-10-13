42a1991fb17e53d667f9f7a5f65eede5694b6705
Arnoldo Gasperi <agasperi@gmail.com>
2021-10-13 17:52:39 -0500

    Script básico de Azure con Powershell (CloudGuru)

---
+++ microsoft/powershell/Azure/createVirtualMachine/create-vm.ps1
@@ -0,0 +1,25 @@
+﻿# Connect to Azure
+Connect-AzAccount
+
+# Variables for common values
+$resourceGroup = "myResourceGroup4"
+$location = "eastus"
+$vmName = "myVM"
+
+# Create user object
+$cred = Get-Credential -Message "Enter a username and password for the virtual machine."
+
+# Create a resource group
+New-AzResourceGroup -Name $resourceGroup -Location $location
+
+# Create a virtual machine
+New-AzVM `
+  -ResourceGroupName $resourceGroup `
+  -Name $vmName `
+  -Location $location `
+  -ImageName "win2016Datacenter" `
+  -SubnetName "mySubnet" `
+  -SecurityGroupName "myNetworkSecurityGroup" `
+  -PublicIpAddressName "myPublicIp" `
+  -Credential $cred `
+  -OpenPorts 3389
---
+++ microsoft/powershell/Azure/firstShell/first.ps1
@@ -0,0 +1,22 @@
+﻿# Welcome to the script
+
+Write-Host "Welcome from Linux Academy!!!"
+
+# Connect to Azure Account
+
+Connect-AzAccount
+
+# Get the date
+
+$date = Get-Date
+Write-Host "Today is " $date
+
+# Get my name
+$name = "Arnoldo Gásperi"
+Write-Host "My name is " $name
+
+# Get all azure Resources
+
+Get-AzResource | Export-Csv -Path C:\Users\Usuario\Dropbox\training\Azure\test.csv
+
+Get-AzResource | ConvertTo-Html | Out-File C:\Users\Usuario\Dropbox\training\Azure\test.html
